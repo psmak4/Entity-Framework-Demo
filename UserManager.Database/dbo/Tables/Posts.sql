592ce378549207a6781e0895dea0786698780d20
Jones <pjones220@cable.comcast.com>
2017-03-09 13:22:18 -0700

    Added database project

---
+++ UserManager.Database/dbo/Tables/Posts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Posts] (
+    [PostId] INT            IDENTITY (1, 1) NOT NULL,
+    [UserId] INT            NOT NULL,
+    [Title]  NVARCHAR (50)  NOT NULL,
+    [Body]   NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([PostId] ASC),
+    CONSTRAINT [FK_Posts_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserId])
+);
+
---
+++ UserManager.Database/dbo/Tables/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]   INT           IDENTITY (1, 1) NOT NULL,
+    [Username] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
